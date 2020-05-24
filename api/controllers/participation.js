6ec0f23390f3e2dcc65900f38e53bda8ce69d5a4
Alcides A <aaugusto.kd@gmail.com>
2020-05-24 16:14:09 -0300

    deleting response helper

--- api/controllers/participation.js
+++ api/controllers/participation.js
@@ -1,16 +1,14 @@
 import Participation from '../models/participation';
 
-import { errorMessage, successMessage } from '../helpers/responseHandler';
-
 export default {
   async list(req, res) {
     try {
       const participations = await Participation.findAll();
 
-      participations && res.json(successMessage(participations));
+      participations && res.json(participations);
     } catch (error) {
       console.log('err', error);
-      return res.json(errorMessage('Erro ao buscar participações'));
+      return res.json('Erro ao buscar participações');
     }
   },
 
@@ -24,11 +22,10 @@ export default {
         participation
       });
 
-      response &&
-        res.json(successMessage('Participação cadastrada com sucesso!'));
+      response && res.json('Participação cadastrada com sucesso!');
     } catch (error) {
       console.log('err', error);
-      return res.json(errorMessage('Erro ao criar participação'));
+      return res.json('Erro ao criar participação');
     }
   }
 };
