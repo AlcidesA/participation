7a8982dd204028ae88667a9d26c89396eedc376c
Alcides A <aaugusto.kd@gmail.com>
2020-05-31 17:59:02 -0300

    api error status code, create participation validation

--- api/controllers/participation.js
+++ api/controllers/participation.js
@@ -8,13 +8,17 @@ export default {
       participations && res.json(participations);
     } catch (error) {
       console.log('err', error);
-      return res.json('Erro ao buscar participações');
+      return res.status(500).json('Erro ao buscar participações');
     }
   },
 
   async create(req, res) {
     try {
       const { firstName, lastName, participation } = req.body;
+
+      if (!(firstName && lastName && participation)) {
+        return res.status(400).json('Erro ao cadastrar participação');
+      }
 
       let response = await Participation.create({
         firstName,
@@ -25,7 +29,7 @@ export default {
       response && res.json('Participação cadastrada com sucesso!');
     } catch (error) {
       console.log('err', error);
-      return res.json('Erro ao criar participação');
+      return res.status(500).json('Erro ao criar participação');
     }
   }
 };
