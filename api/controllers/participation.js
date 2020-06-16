1091616a11b2117ce16c882379381f0ef9f38e97
Alcides A <aaugusto.kd@gmail.com>
2020-06-15 22:09:37 -0300

    api - delete method

--- api/controllers/participation.js
+++ api/controllers/participation.js
@@ -5,7 +5,7 @@ export default {
     try {
       const participations = await Participation.findAll();
 
-      participations && res.json(participations);
+      participations && res.status(200).json(participations);
     } catch (error) {
       console.log('err', error);
       return res.status(500).json('Erro ao buscar participações');
@@ -26,10 +26,25 @@ export default {
         participation
       });
 
-      response && res.json('Participação cadastrada com sucesso!');
+      response && res.status(200).json('Participação cadastrada com sucesso!');
     } catch (error) {
       console.log('err', error);
       return res.status(500).json('Erro ao criar participação');
+    }
+  },
+
+  async delete(req, res) {
+    try {
+      let response = await Participation.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+
+      response && res.status(410).json('Participação removida com sucesso');
+    } catch (error) {
+      console.log('err', error);
+      return res.status(500).json('Erro ao deletar participação');
     }
   }
 };
--- api/routes/participation.js
+++ api/routes/participation.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get('/', participation.list);
 router.post('/', participation.create);
+router.delete('/:id', participation.delete);
 
 export default router;
